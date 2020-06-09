573f0766d7f59857ff2aaffe15f6787804e74fd8
zdsmit <zdsmith001@gmail.com>
2020-06-09 09:32:06 -0700

    added scenario seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Scenario.create(text: "
+  One quiet day, Paul is sitting at home, when he suddenly hears a noise.
+  It sounds like it's coming from outside the house. What does he do?
+")
