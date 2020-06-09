95270817c1e49657e237873879c667b58572d96e
zdsmit <zdsmith001@gmail.com>
2020-06-09 08:35:51 -0700

    added Scenario and Response serializers

---
+++ app/serializers/response_serializer.rb
@@ -0,0 +1,4 @@
+class ResponseSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes 
+end
---
+++ app/serializers/scenario_serializer.rb
@@ -0,0 +1,4 @@
+class ScenarioSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes 
+end
