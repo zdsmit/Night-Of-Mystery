902d4fdd2a91c3c11b032cc462fa72afb19339b8
zdsmit <zdsmith001@gmail.com>
2020-06-12 09:41:41 -0700

    can successfully change scenarios by clicking on specified response

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,18 @@
 const BASE_URL = "http://localhost:3000"
 const SCENARIO_URL = `${BASE_URL}/scenarios`
 
-function getScenario() {
+function getScenario(id) {
   fetch(SCENARIO_URL)
   .then(res => res.json())
   .then(scenarios => {
     scenarios.data.forEach(scenario => {
-      if (scenario.id == 1) {createScenario(scenario)}
+      if (scenario.id == id) {createScenario(scenario)}
     })
   })
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  getScenario()
+  getScenario(1)
 })
 
 function createScenario(scenario) {
@@ -21,6 +21,7 @@ function createScenario(scenario) {
   let p = document.createElement("p")
   p.setAttribute("class", "scenario_text")
   p.innerText = scenario.attributes.text
+  div.appendChild(p)
 
   let ol = document.createElement("ol")
   scenario.attributes.responses.forEach(response => {
@@ -30,11 +31,10 @@ function createScenario(scenario) {
 
     li.addEventListener("click", function() {
       div.removeChild(p)
-      
+      getScenario(response.result)
     })
   })
 
   p.appendChild(ol)
-  div.appendChild(p)
   document.querySelector("main").appendChild(div)
 }
